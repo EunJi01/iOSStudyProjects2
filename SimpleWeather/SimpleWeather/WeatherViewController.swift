4177b2517c45ffad60434084d86c8860feff78b1
EunJi01 <hej0102@naver.com>
2022-05-26 19:32:16 +0900

    SimpleWeather
    Viewcontroller 구성

--- SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -9,7 +9,28 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var cityLable: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    let cities = ["Seoul", "Tokyo", "LA", "Seattle"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        
+        cityLable.text = cities.randomElement()
+        
+        let imageName =  weathers.randomElement()!
+        
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLable.text = "\(randomTemp)°"
+        
+        print("도시, 온도, 날씨, 이미지 변경하자!")
     }
 }
