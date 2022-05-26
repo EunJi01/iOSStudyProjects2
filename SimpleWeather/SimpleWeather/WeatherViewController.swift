5f7a96724e1ffe35bcc407c832f406544e6b64c8
EunJi01 <hej0102@naver.com>
2022-05-26 19:08:07 +0900

    SimpleWeather
    Storyboard 구성

---
+++ SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by 황은지 on 2022/05/26.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
