d56e78dca99bfbcafcc1090569f5b31e6466ae1d
EunJi01 <hej0102@naver.com>
2022-05-27 21:32:44 +0900

    StockRank

---
+++ 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by 황은지 on 2022/05/27.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
