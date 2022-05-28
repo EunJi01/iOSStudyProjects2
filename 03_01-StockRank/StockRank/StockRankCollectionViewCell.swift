8391f73736990d97c9a80298417071f1b752553b
EunJi01 <hej0102@naver.com>
2022-05-28 13:47:24 +0900

    StockRank

--- 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
+++ 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class StockRankCollectionViewCell: UICollectionViewCell {
     
+    // uicomponent 연결하자
+    // uicomponent 에 데이터를 업데이트 하는 코드를 넣자
+    
+    @IBOutlet weak var rankLable: UILabel!
+    @IBOutlet weak var companyIconImageView: UIImageView!
+    @IBOutlet weak var companyNameLable: UILabel!
+    @IBOutlet weak var companyPriceLable: UILabel!
+    @IBOutlet weak var diffLable: UILabel!
+    
+    func configure(_ stock: StockModel) {
+        rankLable.text = "\(stock.rank)"
+        companyIconImageView.image = UIImage(named: stock.imageName)
+        companyNameLable.text = stock.name
+        companyPriceLable.text = "\(stock.price) 원"
+        diffLable.text = "\(stock.diff)%"
+    }
 }
--- 03_01-StockRank/StockRank/StockRankViewController.swift
+++ 03_01-StockRank/StockRank/StockRankViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class StockRankViewController: UIViewController {
 
+    let stockList: [StockModel] = StockModel.list
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    // UiCollectionView 사용 시 고려할 것: Data, Presentation, Layout
+    // Data - 어떤 데이를 사용할지? -> stockList
+    // Presentation - 셀을 어떻게 표현할지?
+    // Layout - 셀들 레이아웃을 어떻게 할지?
+    
+    // protocol -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+    }
+ }
 
-        // Do any additional setup after loading the view.
+extension StockRankViewController: UICollectionViewDataSource {
+    
+    // collectionView에 몇개의 셀이 필요한가?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
     }
     
+    // collectionView의 각 셀을 어떻게 표현하는가?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath) as? StockRankCollectionViewCell else {
+            return UICollectionViewCell()
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let stock = stockList[indexPath.item]
+        cell.configure(stock)
+        return cell
     }
-    */
+}
 
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // width = collectionView
+        // height = 80
+        
+        return CGSize(width: collectionView.bounds.width, height: 80)
+    }
 }
