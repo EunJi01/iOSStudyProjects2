2c2e3834b815d78a338c778c254b1e2d641a6279
EunJi01 <hej0102@naver.com>
2022-06-03 18:59:21 +0900

    fix: stockrank

--- 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
+++ 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -26,12 +26,12 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         companyPriceLable.text = "\(convertToCurrencyFormat(price: stock.price)) 원"
         
         // if 문 사용
-//        if stock.diff >= 0 {
-//            diffLable.textColor = .red
-//        }
-//        else {
-//            diffLable.textColor = .blue
-//        }
+        //        if stock.diff >= 0 {
+        //            diffLable.textColor = .red
+        //        }
+        //        else {
+        //            diffLable.textColor = .blue
+        //        }
         
         // 삼항연산자 사용
         diffLable.textColor = stock.diff >= 0 ? .red : .blue
@@ -47,20 +47,19 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         return result
     }
     
-    var lButton :Bool = true
     
-         // likeButton을 탭하였을 때 색상을 변경시켜보자
-    @IBAction func changeLikeButtonTapped(_ sender: Any) {
-            print("버튼 눌림")
+    // likeButton을 탭하였을 때 색상을 변경시켜보자
+    var lButton :Bool = false
     
-            lButton = !lButton
-    
-            if lButton {
-                likeButton.setImage(UIImage(named: "heart"), for: .normal)
-            } else {
-                likeButton.setImage(UIImage(named: "heart.fill"), for: .normal)
-            }
-    
-            print("변경 완료")
+    @IBAction func changeLikeButtonTapped(_ sender: UIButton) {
+        if lButton {
+            lButton = false
+            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        } else {
+            lButton = true
+            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
         }
+        
+        print("변경 완료")
+    }
 }
