f4db29dec59253674b31be3a75c667e9ed684cf8
EunJi01 <hej0102@naver.com>
2022-06-10 19:38:56 +0900

    Feat: thumbnail cornerRadius

--- 03_02-ChatList/ChatList/ChatListCollectionViewCell.swift
+++ 03_02-ChatList/ChatList/ChatListCollectionViewCell.swift
@@ -14,6 +14,14 @@ class ChatListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var chatLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
         
+    // dequeue할 때(VC에서 Cell의 정보를 하나씩 받아올 때) 가장 먼저 호출되는 함수
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // thumnail의 모서리를 둥글게 바꿈
+        thumbnail.layer.cornerRadius = 20
+    }
+    
     func configure(_ chat: Chat) {
         thumbnail.image = UIImage(named: chat.name)
         nameLabel.text = chat.name
